f4f684d7489098a1d2c9da7f84e010508985ff81
jm <root1@root.local>
2020-09-09 11:44:31 -0700

    tool check command searches all volumes and determines if backup volume is connected, and what the path is for the volume.

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ log(chalk.green.bgGrey("Backup Tool"))
 
 program.version("0.0.1")
 
+let backupVolume = 'volume not found'
+let backupPath = 'path not found'
+
+function getVolume(){
+
+}
+
 program
   .command("check")
   .description("Check if backup volume is mounted")
   .action(async () => {
     let drives = await dl.list()
-    log(drives)
+    drives.forEach((drive,index)=>{
+        log(drive.mountpoints)
+        if(drive.mountpoints[0].label === 'rootext'){
+            backupVolume = drive
+            backupPath = drive.mountpoints[0].path
+            log(chalk.green('Backup volume found: ') + chalk.cyan(JSON.stringify(backupVolume)) + ' at path: ' + chalk.green(backupPath))
+            return
+        }
+        log(chalk.red('Backup volume not found.'))
+    })
   })
 
 program
